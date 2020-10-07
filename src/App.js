28958c9de4037ad43be9a80d534c262d5b56fd06
Justin Langlinais <justinclanglinais@gmail.com>
2020-10-07 13:02:42 -0700

    feat(App): start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [armbars, setArmbars] = useState(0);
+  const [triangles, setTriangles] = useState(0);
+  const [omoplatas, setOmoplatas] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Armbars armbars={armbars} />
+      <Trianges triangles={triangles} />
+      <Omoplatas omoplatas={omoplatas} />
     </div>
   );
 }
